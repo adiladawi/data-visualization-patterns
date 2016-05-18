b250d5cc593e85f9d4fde454135f8b851f04d7b5
Arturo Mora-Soto <jarturomora@gmail.com>
2016-05-18 14:01:05 -0500

    Some changes to wiki integration script

--- wiki_integration/create_wiki_page.py
+++ wiki_integration/create_wiki_page.py
@@ -2,23 +2,23 @@ import sys
 
 def create_wiki_page(params):
     """Open the three markdown files of a pattern and create the Wiki page""" 
-    main_page = open(params[0]).read()
-    py_page = open(params[1]).read()
-    r_page = open(params[2]).read()
+    main_page = open(params[4] + params[0]).read()
+    py_page = open(params[4] + params[1]).read()
+    r_page = open(params[4] + params[2]).read()
 
-    wiki_page = open(params[3], 'w')
+    wiki_page = open(params[5] + params[3], 'w')
     wiki_page.write(main_page)
     wiki_page.write(py_page)
     wiki_page.write(r_page)
 
     wiki_page.close()
-    print "Wiki page '%s' successful created." % (params[3])
+    print "Wiki page '%s' successfully created." % (params[3])
 
 def main():
-    if len(sys.argv) != 5:
+    if len(sys.argv) != 7:
         print "usage: ./create_wiki_page.py main_wiki, py_wiki, r_wiki file_name"
     else:
-        create_wiki_page(sys.argv[1:5])
+        create_wiki_page(sys.argv[1:7])
 
 
 
