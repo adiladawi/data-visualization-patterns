827fc01c4dba63462de04b64d09558534ff6c3dd
Arturo Mora-Soto <jarturomora@gmail.com>
2016-05-30 13:27:34 -0500

    create_wiki_page.py improved by reading the files structure from a configuration file

--- wiki_integration/create_wiki_page.py
+++ wiki_integration/create_wiki_page.py
@@ -1,26 +1,29 @@
 import sys
 
-def create_wiki_page(params):
+def create_wiki_page(config_file):
     """Open the three markdown files of a pattern and create the Wiki page""" 
-    main_page = open(params[4] + params[0]).read()
-    py_page = open(params[4] + params[1]).read()
-    r_page = open(params[4] + params[2]).read()
+    
+    # Open configuracion file
+    conf = [line.rstrip() for line in open(config_file)]
+    files = {conf[i]:conf[i + 1] for i in xrange(0, len(conf), 2)}
+    
+    main_page = open(files['source_path'] + files['main_page']).read()
+    py_page = open(files['source_path'] + files['py_page']).read()
+    r_page = open(files['source_path'] + files['r_page']).read()
 
-    wiki_page = open(params[5] + params[3], 'w')
+    wiki_page = open(files['dest_path'] + files['new_page'], 'w')
     wiki_page.write(main_page)
     wiki_page.write(py_page)
     wiki_page.write(r_page)
 
     wiki_page.close()
-    print "Wiki page '%s' successfully created." % (params[3])
+    print "Wiki page '%s' successfully created." % (files['new_page'])
 
 def main():
-    if len(sys.argv) != 7:
-        print "usage: ./create_wiki_page.py main_wiki, py_wiki, r_wiki file_name"
+    if len(sys.argv) != 2:
+        print "usage: ./create_wiki_page.py wiki_config.txt"
     else:
-        create_wiki_page(sys.argv[1:7])
-
-
+        create_wiki_page(sys.argv[1])
 
 if __name__ == '__main__':
     main()
